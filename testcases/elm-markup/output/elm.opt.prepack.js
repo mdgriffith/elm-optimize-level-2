6e54f26d0d9a87739101256b6f8bcd9e0e8a48e2
mdgriffith <mdgriffith@users.noreply.github.com>
2020-08-01 11:00:21 -0400

    compiled elm code for elm-markup

---
+++ testcases/elm-markup/output/elm.opt.prepack.js
@@ -0,0 +1,22 @@
+(function () {
+  var $$0_enumerable_58false_44configurable_58true_44writable_58true = {
+    enumerable: false,
+    configurable: true,
+    writable: true
+  };
+
+  var _$0_global = this;
+
+  var _$1_TypeError = _$0_global.TypeError;
+  var _$2_TypeError_46prototype = _$1_TypeError.prototype;
+  var _$3_Object = _$0_global.Object;
+  var _$4_Object_46defineProperty = _$3_Object.defineProperty;
+
+  var __constructor = function () {};
+
+  var _0_ = (__constructor.prototype = _$2_TypeError_46prototype, new __constructor());
+
+  $$0_enumerable_58false_44configurable_58true_44writable_58true.value = "attempt to change immutable binding", _$4_Object_46defineProperty(_0_, "message", $$0_enumerable_58false_44configurable_58true_44writable_58true);
+  $$0_enumerable_58false_44configurable_58true_44writable_58true.value = "TypeError: attempt to change immutable binding\n    at testcases/elm-markup/output/elm.opt.transformed.js:5066:46\n    at testcases/elm-markup/output/elm.opt.transformed.js:1:2", _$4_Object_46defineProperty(_0_, "stack", $$0_enumerable_58false_44configurable_58true_44writable_58true);
+  throw _0_;
+}).call(this);
