814af8b5ef431a798022fd828ce70a733fd25141
mdgriffith <mdgriffith@users.noreply.github.com>
2020-08-08 12:30:15 -0400

    simplify virtualDom correction.

--- src/experiments/correctVirtualDom.ts
+++ src/experiments/correctVirtualDom.ts
@@ -1,107 +1,81 @@
 import ts from 'typescript';
 
-const newVDomNode = `_VirtualDom_node = F3(function (tag, factList, kidList) {
-    for (var kids = [], descendantsCount = 0; kidList.b; kidList = kidList.b) // WHILE_CONS
-        {
-        var kid = kidList.a;
-        descendantsCount += (kid.b || 0);
-        kids.push(kid);
-    }
-    descendantsCount += kids.length;
-    return {
-        $: 1,
-        c: tag,
-        d: _VirtualDom_organizeFacts(factList),
-        e: kids,
-        f: undefined,
-        b: descendantsCount
-    };
+const newVDomNS4 = `_VirtualDom_nodeNS = F4(function (namespace, tag, factList, kidList) {
+      for (var kids = [], descendantsCount = 0; kidList.b; kidList = kidList.b) // WHILE_CONS
+       {
+          var kid = kidList.a;
+          descendantsCount += (kid.b || 0);
+          kids.push(kid);
+      }
+      descendantsCount += kids.length;
+      return {
+          $: 1,
+          c: tag,
+          d: _VirtualDom_organizeFacts(factList),
+          e: kids,
+          f: namespace,
+          b: descendantsCount
+      };
+
 })`;
 
-const prename = `_VirtualDom_node_prename = function (tag){ 
-    return F2(function (factList, kidList) {
-        for (var kids = [], descendantsCount = 0; kidList.b; kidList = kidList.b) // WHILE_CONS
-         {
-            var kid = kidList.a;
-            descendantsCount += (kid.b || 0);
-            kids.push(kid);
-        }
-        descendantsCount += kids.length;
-        return {
-            $: 1,
-            c: tag,
-            d: _VirtualDom_organizeFacts(factList),
-            e: kids,
-            f: undefined,
-            b: descendantsCount
-        };
-    });
-}`;
-
-// $elm$virtual_dom$VirtualDom$node
-// tag == "script" ? "p" : tag
-
-const apiNewVDomNode = `$elm$virtual_dom$VirtualDom$node = F3(function (tag, factList, kidList) {
-    for (var kids = [], descendantsCount = 0; kidList.b; kidList = kidList.b) // WHILE_CONS
-        {
-        var kid = kidList.a;
-        descendantsCount += (kid.b || 0);
-        kids.push(kid);
-    }
-    descendantsCount += kids.length;
-    return {
-        $: 1,
-        c: tag === "script" ? "p" : tag,
-        d: _VirtualDom_organizeFacts(factList),
-        e: kids,
-        f: undefined,
-        b: descendantsCount
-    };
-})`;
-
-const extractAstFromCode = (sourceText: string): ts.Node => {
+const ast = (sourceText: string): ts.Node => {
   const source = ts.createSourceFile('bla', sourceText, ts.ScriptTarget.ES2018);
 
   return source.statements[0];
 };
 
+function create(name: string, body: ts.Node): ts.Node {
+  if (
+    ts.isExpressionStatement(body) &&
+    ts.isBinaryExpression(body.expression)
+  ) {
+    return ts.createVariableDeclaration(name, undefined, body.expression.right);
+  }
+  return body;
+}
+
 export const replaceVDomNode = (): ts.TransformerFactory<ts.SourceFile> => context => {
   return sourceFile => {
     const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+      // replace the two virtualdom node functions with our own implementation
       if (ts.isVariableDeclaration(node)) {
         if (
           ts.isIdentifier(node.name) &&
-          node.name.text == '_VirtualDom_node'
+          node.name.text == '_VirtualDom_nodeNS'
         ) {
-          let body = extractAstFromCode(newVDomNode);
-          let prenamedNode = extractAstFromCode(prename);
-
-          if (
-            ts.isExpressionStatement(body) &&
-            ts.isBinaryExpression(body.expression)
-          ) {
-            return ts.createVariableDeclaration(
-              '_VirtualDom_node',
-              undefined,
-              body.expression.right
-            );
-          }
-        } else if (
-          ts.isIdentifier(node.name) &&
-          node.name.text == '$elm$virtual_dom$VirtualDom$node'
-        ) {
-          let body = extractAstFromCode(apiNewVDomNode);
-          if (ts.isExpressionStatement(body)) {
-            if (ts.isBinaryExpression(body.expression)) {
-              return ts.createVariableDeclaration(
-                '$elm$virtual_dom$VirtualDom$node',
-                undefined,
-                body.expression.right
-              );
-            }
-          }
+          return create('_VirtualDom_nodeNS', ast(newVDomNS4));
         }
       }
+
+      // replace calls
+      //   var $elm$html$Html$div = _VirtualDom_node("div");
+      // with
+      //   var $elm$html$Html$div = _VirtualDom_nodeNS(undefined, "div");
+      if (
+        ts.isCallExpression(node) &&
+        ts.isIdentifier(node.expression) &&
+        node.expression.text == '_VirtualDom_node' &&
+        node.arguments.length == 1
+      ) {
+        return ts.createCall(ts.createIdentifier('A2'), undefined, [
+          ts.createIdentifier('_VirtualDom_nodeNS'),
+          ts.createIdentifier('undefined'),
+          node.arguments[0],
+        ]);
+      }
+
+      // replace calls to $elm$virtual_dom$VirtualDom$node with calls to _VirtualDom_nodeNS
+      const replaced = replaceVDomWithNSInline(node);
+      if (replaced) {
+        return replaced;
+      }
+
+      // An alternative version of the above which is slower.
+      // const replaced = replaceAPIVDomNodeWithF3A4(node)
+      // if (replaced) {
+      //   return replaced
+      // }
 
       return ts.visitEachChild(node, visitor, context);
     };
@@ -109,3 +83,86 @@ export const replaceVDomNode = (): ts.TransformerFactory<ts.SourceFile> => conte
     return ts.visitNode(sourceFile, visitor);
   };
 };
+
+// replace calls to $elm$virtual_dom$VirtualDom$node with calls to _VirtualDom_nodeNS
+function replaceVDomWithNSInline(node: ts.Node): ts.Node | undefined {
+  if (
+    ts.isCallExpression(node) &&
+    node.expression &&
+    ts.isIdentifier(node.expression) &&
+    node.expression.text === 'A3'
+  ) {
+    if (
+      ts.isIdentifier(node.arguments[0]) &&
+      node.arguments[0].text === '$elm$virtual_dom$VirtualDom$node'
+    ) {
+      return ts.createCall(ts.createIdentifier('A4'), undefined, [
+        ts.createIdentifier('_VirtualDom_nodeNS'),
+        ts.createIdentifier('undefined'),
+        ts.createCall(ts.createIdentifier('_VirtualDom_noScript'), undefined, [
+          node.arguments[1],
+        ]),
+        node.arguments[2],
+        node.arguments[3],
+      ]);
+    }
+  }
+
+  return undefined;
+}
+
+// replace calls
+//    var $elm$virtual_dom$VirtualDom$node = function (tag) {
+//        return _VirtualDom_node(_VirtualDom_noScript(tag));
+//    };
+// with
+//   var $elm$virtual_dom$VirtualDom$node = _VirtualDom_nodeNS(undefined, "div");
+function replaceAPIVDomNodeWithF3A4(node: ts.Node): ts.Node | undefined {
+  if (
+    ts.isVariableDeclaration(node) &&
+    ts.isIdentifier(node.name) &&
+    node.name.text == '$elm$virtual_dom$VirtualDom$node' &&
+    node.initializer &&
+    ts.isFunctionExpression(node.initializer)
+  ) {
+    let newCall = ts.createCall(ts.createIdentifier('A4'), undefined, [
+      ts.createIdentifier('_VirtualDom_nodeNS'),
+      ts.createIdentifier('undefined'),
+      ts.createCall(ts.createIdentifier('_VirtualDom_noScript'), undefined, [
+        ts.createIdentifier('tag'),
+      ]),
+      ts.createIdentifier('attrs'),
+      ts.createIdentifier('kids'),
+    ]);
+
+    let newFn = ts.createCall(ts.createIdentifier('F3'), undefined, [
+      ts.createFunctionExpression(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        [param('tag'), param('attrs'), param('kids')],
+        undefined,
+        ts.createBlock([ts.createReturn(newCall)])
+      ),
+    ]);
+
+    return ts.createVariableDeclaration(
+      '$elm$virtual_dom$VirtualDom$node',
+      undefined,
+      newFn
+    );
+  }
+}
+
+function param(name: string) {
+  return ts.createParameter(
+    undefined,
+    undefined,
+    undefined,
+    ts.createIdentifier(name),
+    undefined,
+    undefined,
+    undefined
+  );
+}
