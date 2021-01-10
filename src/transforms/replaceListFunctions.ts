36e5882e9072784c6847cab37f8a067aa373b0fe
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-01-10 15:12:47 +0000

    More list functions

--- src/transforms/replaceListFunctions.ts
+++ src/transforms/replaceListFunctions.ts
@@ -14,8 +14,147 @@ var $elm$core$List$map = F2(function(f, xs) {
 });
 `;
 
+const $elm$core$List$indexedMap = `
+var $elm$core$List$indexedMap = F2(function (f, xs) {
+  var tmp = _List_Cons(undefined, _List_Nil);
+  var end = tmp;
+  for (var i = 0; xs.b; i++) {
+    var next = _List_Cons(A2(f, i, xs.a), _List_Nil);
+    end.b = next;
+    end = next;
+    xs = xs.b;
+  }
+  return tmp.b;
+});
+`;
+
+const $elm$core$List$filter = `
+var $elm$core$List$filter = F2(function (f, xs) {
+  var tmp = _List_Cons(undefined, _List_Nil);
+  var end = tmp;
+  while (xs.b) {
+    if (f(xs.a)) {
+      var next = _List_Cons(xs.a, _List_Nil);
+      end.b = next;
+      end = next;
+    }
+    xs = xs.b;
+  }
+  return tmp.b;
+});
+`;
+
+const $elm$core$List$append = `
+var $elm$core$List$append = F2(function (xs, ys) {
+  var tmp = _List_Cons(undefined, _List_Nil);
+  var end = tmp;
+  while (xs.b) {
+    var next = _List_Cons(xs.a, _List_Nil);
+    end.b = next;
+    end = next;
+    xs = xs.b;
+  }
+  end.b = ys;
+
+  return tmp.b;
+});`;
+
+const $elm$core$List$concat = `
+var $elm$core$List$concat = function (lists) {
+  var tmp = _List_Cons(undefined, _List_Nil);
+  var end = tmp;
+  if (!lists.b) {
+    return _List_Nil;
+  }
+  for (; lists.b.b; lists = lists.b) {
+    var xs = lists.a;
+    while (xs.b) {
+      var next = _List_Cons(xs.a, _List_Nil);
+      end.b = next;
+      end = next;
+      xs = xs.b;
+    }
+  }
+  end.b = lists;
+
+  return tmp.b;
+};`;
+
+const $elm$core$List$intersperse = `
+var $elm$core$List$intersperse = F2(function (sep, xs) {
+  if (!xs.b) {
+    return xs;
+  }
+  var tmp = _List_Cons(undefined, _List_Nil);
+  var end = tmp;
+
+  end.b = _List_Cons(xs.a, _List_Nil);
+  end = end.b;
+  xs = xs.b;
+
+  while (xs.b) {
+    var valNode = _List_Cons(xs.a, _List_Nil);
+    var sepNode = _List_Cons(sep, valNode);
+    end.b = sepNode;
+    end = valNode;
+    xs = xs.b;
+  }
+
+  return tmp.b;
+});`;
+
+const $elm$core$List$partition = `
+var $elm$core$List$partition = F2(function (f, xs) {
+  var truesHead = _List_Cons(undefined, _List_Nil);
+  var falsesHead = _List_Cons(undefined, _List_Nil);
+  var truesEnd = truesHead;
+  var falsesEnd = falsesHead;
+  while (xs.b) {
+    var next = _List_Cons(xs.a, _List_Nil);
+    if (f(xs.a)) {
+      truesEnd.b = next;
+      truesEnd = next;
+    } else {
+      falsesEnd.b = next;
+      falsesEnd = next;
+    }
+    xs = xs.b;
+  }
+  return _Utils_Tuple2(truesHead.b, falsesHead.b);
+});`;
+
+const $elm$core$List$unzip = `
+var $elm$core$List$unzip = function (pairs) {
+  var aHead = _List_Cons(undefined, _List_Nil);
+  var bHead = _List_Cons(undefined, _List_Nil);
+  var aEnd = aHead;
+  var bEnd = bHead;
+  while (pairs.b) {
+    var tuple = pairs.a;
+
+    var aNext = _List_Cons(tuple.a, _List_Nil);
+    aEnd.b = aNext;
+    aEnd = aNext;
+
+    var bNext = _List_Cons(tuple.b, _List_Nil);
+    bEnd.b = bNext;
+    bEnd = bNext;
+
+    pairs = pairs.b;
+  }
+  return _Utils_Tuple2(aHead.b, bHead.b);
+};`;
+
+
 const replacements = {
   $elm$core$List$map,
+  $elm$core$List$indexedMap,
+  $elm$core$List$filter,
+  $elm$core$List$append,
+  $elm$core$List$concat,
+  $elm$core$List$intersperse,
+  $elm$core$List$partition,
+  $elm$core$List$unzip,
 };
 
 export const replaceListFunctions: ts.TransformerFactory<ts.SourceFile> = (
