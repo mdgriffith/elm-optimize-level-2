39018cac7a1582f081f42952509242a6608e11f4
mdgriffith <mdgriffith@users.noreply.github.com>
2021-01-22 19:59:33 -0500

    v8 analyze transformations

--- src/transforms/analyze.ts
+++ src/transforms/analyze.ts
@@ -36,92 +36,9 @@ This transformer does a few things.
 
 import ts from 'typescript';
 import {ast, create} from './utils/create';
-
-const BENCHMARK_SAMPLE_FN = '_Benchmark_sample';
-const benchmarkSampleWithAnalysis = `_Benchmark_sample = F2(function(n, fn) {
-    return __Scheduler_binding(function(callback) {
-        var start = _Benchmark_getTimestamp();
-
-        try {
-            for (var i = 0; i < n; i++) {
-                fn();
-            }
-        } catch (error) {
-            if (error instanceof RangeError) {
-                callback(__Scheduler_fail(__BL_StackOverflow));
-            } else {
-                callback(__Scheduler_fail(__BL_UnknownError(error.message)));
-            }
-            return;
-        }
-        if (v8.isNative()) {
-            var fn_name = v8.functionGetName(fn);
-            console.log(fn_name + ' opt status: ', v8.getOptimizationStatus(fn));
-
-            // v8.helpers.testOptimization(fn);
-            // console.log(fn_name + ' opt status: ', v8.getOptimizationStatus(fn));
-            
-        } else {
-            console.log('You need to start Chrome with the --js-flags="--allow-natives-syntax"')
-        }
-
-        var end = _Benchmark_getTimestamp();
-
-        callback(__Scheduler_succeed(end - start));
-    });
-})`;
+import {createCallGraph, print, getCalled} from './utils/callgraph';
 
 
-/*
-
-This is the function in `elm-benchmark`
-
-    // sample : Int -> Operation -> Task Error Float
-    var _Benchmark_sample = F2(function(n, fn) {
-        return __Scheduler_binding(function(callback) {
-            var start = _Benchmark_getTimestamp();
-
-            try {
-                for (var i = 0; i < n; i++) {
-                    fn();
-                }
-            } catch (error) {
-            if (error instanceof RangeError) {
-                callback(__Scheduler_fail(__BL_StackOverflow));
-            } else {
-                callback(__Scheduler_fail(__BL_UnknownError(error.message)));
-            }
-            return;
-            }
-
-            var end = _Benchmark_getTimestamp();
-
-            callback(__Scheduler_succeed(end - start));
-        });
-    });
-
-We want to replace it with
-
-
-
-*/
-export const benchmarkFunctionAnalyzer = (): ts.TransformerFactory<ts.SourceFile> => context => {
-    return sourceFile => {
-        const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
-            if (ts.isVariableDeclaration(node)) {
-                if (
-                    ts.isIdentifier(node.name) &&
-                    node.name.text == BENCHMARK_SAMPLE_FN
-                ) {
-                    return create(BENCHMARK_SAMPLE_FN, ast(benchmarkSampleWithAnalysis));
-                }
-            }
-            return ts.visitEachChild(node, visitor, context);
-        };
-        return ts.visitNode(sourceFile, visitor);
-    };
-};
-  
 /*
 
 The stubbed version of the functions are:
@@ -139,47 +56,123 @@ The stubbed version of the functions are:
 */
 
 
-const DEBUG_ANALYZE_FN = '$author$project$Debug$V8$analyze';
-const new_analyze = `$author$project$Debug$V8$analyze = F2(function (tag, value) {
-    if (v8.isNative()) {
+const DEBUG_MEMORY_FN = '$author$project$Debug$V8$memory';
+const new_memory = `$author$project$Debug$V8$memory = F2(function (tag, value) {
+    console.log("TEST")
+    if (v8 && v8.isNative()) {
+        // console.log("MEMORY ANALYSIS", value)
         var analysis = {
-            v8.hasFastProperties: hasFastProperties(value),
-            v8.hasFastSmiElements: hasFastSmiElements(value),
-            v8.hasFastObjectElements: hasFastObjectElements(value),
-            v8.hasFastDoubleElements: hasFastDoubleElements(value),
-            v8.hasDictionaryElements: hasDictionaryElements(value),
-            v8.hasFastHoleyElements: hasFastHoleyElements(value),
-            v8.isValidSmi: isValidSmi(value),
-            v8.isSmi: isSmi(value),
-            v8.hasFastSmiOrObjectElements: hasFastSmiOrObjectElements(value),
-            v8.hasSloppyArgumentsElements: hasSloppyArgumentsElements(value)
+            hasFastProperties: v8.hasFastProperties(value),
+            // hasFastSmiElements: v8.hasFastSmiElements(value),
+            // hasFastObjectElements: v8.hasFastObjectElements(value),
+            // hasFastDoubleElements: v8.hasFastDoubleElements(value),
+            hasDictionaryElements: v8.hasDictionaryElements(value),
+            // hasFastHoleyElements: v8.hasFastHoleyElements(value),
+            // isValidSmi: v8.isValidSmi(value),
+            isSmi: v8.isSmi(value),
+            // hasFastSmiOrObjectElements: v8.hasFastSmiOrObjectElements(value),
+            hasSloppyArgumentsElements: v8.hasSloppyArgumentsElements(value)
         }
-        console.log(tag, v8.getOptimizationStatus(fn));
+        // console.log(tag, analysis);
     }
     return value;
 })`;
 
+const DEBUG_OPT_STATUS_FN = '$author$project$Debug$V8$optimizationStatus';
+const new_opt_status = `$author$project$Debug$V8$optimizationStatus = F2(function (tag, fn) {
+    if (v8.isNative()) {
+        var opt_status = v8.getOptimizationStatus(fn);
+        var binary = opt_status.toString(2).padStart(12, '0');
+        // console.log("FN NAME", fn.toString());
+        console.log(tag, binary);
+    }
+    return fn;
+})`;
 
-const DEBUG_REPORT_FN = '$author$project$Debug$V8$report';
-const reportAnalysis = `$author$project$Debug$V8$report = function (_v0) {
-    return $elm$json$Json$Encode$null;
-}`;
 
 
-export const v8MemoryRepresentation = (): ts.TransformerFactory<ts.SourceFile> => context => {
+// Reporting status automatically for benchmarked fns
+const DEBUG_REPORT_FN = '$author$project$Debug$V8$reportV8StatusForBenchmarks';
+const reportAnalysis = function(fns: string[]) {
+    let fns_string = "";
+    
+    for (const fn_name of fns) {
+        fns_string = fns_string + "{ name: \"" + fn_name + "\", ref:" +  fn_name + "}"
+    }
+    
+
+    return `$author$project$Debug$V8$reportV8StatusForBenchmarks = function (_v0) {
+    var fns = [ ${fns_string} ]
+    var results = {}
+    if (v8.isNative()) {
+        for (const fn of fns) {
+            var opt_status = v8.getOptimizationStatus(fn.ref);
+            var binary = opt_status.toString(2).padStart(12, '0');
+            var tag = "unknown"
+
+            if (binary == "000000000001") {
+                tag = "uncalled"
+            } else if (binary == "000000110001") {
+                tag = "optimized"
+            } else if (binary == "000001000001") {
+                tag = "interpreted"
+            }
+
+
+            results[fn.name] = binary + "(" + tag + ")";
+        }
+    }
+    return results;
+}`
+}
+
+
+export const v8Debug: ts.TransformerFactory<ts.SourceFile> = context => {
     return sourceFile => {
         const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
             if (ts.isVariableDeclaration(node)) {
                 if (
                     ts.isIdentifier(node.name) &&
-                    node.name.text == DEBUG_ANALYZE_FN
+                    node.name.text == DEBUG_MEMORY_FN
                 ) {
-                    return create(DEBUG_ANALYZE_FN, ast(new_analyze));
+                    return create(DEBUG_MEMORY_FN, ast(new_memory));
                 } else if (
+                    ts.isIdentifier(node.name) &&
+                    node.name.text == DEBUG_OPT_STATUS_FN
+                ) {
+                    return create(DEBUG_OPT_STATUS_FN, ast(new_opt_status));
+                } 
+            }
+            return ts.visitEachChild(node, visitor, context);
+        };
+        return ts.visitNode(sourceFile, visitor);
+    };
+};
+
+
+
+
+
+
+export const reportFunctionStatusInBenchmarks: ts.TransformerFactory<ts.SourceFile> = context => {
+    return sourceFile => {
+        const callgraph = createCallGraph(sourceFile)
+
+        let called = getCalled(callgraph, "$author$project$Main$suite", undefined)
+
+        // The callgraph creator gets a little confused in some places, so you can manually stub in function names if you want:
+        called = called.concat(["_VirtualDom_nodeNS_fn",
+            "$author$project$Main$viewLevels_fn",
+            "$author$project$Main$viewLevels", 
+            "$elm$html$Html$div.f"])
+
+        const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+            if (ts.isVariableDeclaration(node)) {
+                if (
                     ts.isIdentifier(node.name) &&
                     node.name.text == DEBUG_REPORT_FN
                 ) {
-                    return create(DEBUG_REPORT_FN, ast(reportAnalysis));
+                    return create(DEBUG_REPORT_FN, ast(reportAnalysis(called)));
                 }
             }
             return ts.visitEachChild(node, visitor, context);
@@ -187,3 +180,4 @@ export const v8MemoryRepresentation = (): ts.TransformerFactory<ts.SourceFile> =
         return ts.visitNode(sourceFile, visitor);
     };
 };
+  
