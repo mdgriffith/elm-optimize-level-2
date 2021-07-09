a3b90e9b1295c00a05144a4a99f636b931a20574
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2021-07-09 09:14:48 +0200

    Allow replacing a function/def with a whole bunch of code.

--- src/transforms/utils/create.ts
+++ src/transforms/utils/create.ts
@@ -3,7 +3,7 @@ import ts from 'typescript';
 
 export const ast = (sourceText: string): ts.Node => {
     const source = ts.createSourceFile('bla', sourceText, ts.ScriptTarget.ES2018);
-    return source.statements[0];
+    return source.statements;
 };
 
 export function create(name: string, body: ts.Node): ts.Node {
