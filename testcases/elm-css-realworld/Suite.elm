31dae249518723a2a54bcaf3157b12de7119a84f
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2021-09-12 19:14:55 +0200

    Add boilerplate for new testcase. Seems there is a bug somewhere, as benchmark doesnt complete...

---
+++ testcases/elm-css-realworld/Suite.elm
@@ -0,0 +1,26 @@
+module Suite exposing (suite)
+
+{-| -}
+
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes as Attr
+import V8.Benchmark.Runner.Json exposing (..)
+
+
+suite : Benchmark
+suite =
+    describe "Elm Css"
+        [ benchmark "Search form inspired by vy.no" view
+        ]
+
+
+view : () -> Html a
+view _ =
+    Html.div
+        []
+        []
+
+
+
+-- BUTTON
+-- TEXT INPUT
