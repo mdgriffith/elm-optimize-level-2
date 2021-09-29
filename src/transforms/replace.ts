ee9a956c9f0afbcf7ead389f6277bfe803361539
Matthew Griffith <mdg.griffith@gmail.com>
2021-09-29 18:41:28 -0400

    don't use `in`, use hasOwnProperty to avoid weird collisions

--- src/transforms/replace.ts
+++ src/transforms/replace.ts
@@ -10,13 +10,13 @@ export const replace = (
         const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
             if (ts.isVariableStatement(node)) {
               const name = node.declarationList.declarations[0]?.name;
-              if (isIdentifier(name) && name.text in replacements) {
+              if (isIdentifier(name) && replacements.hasOwnProperty(name.text)) {
                 const key = name.text as keyof typeof replacements;
                 return ast(replacements[key]);
               }
             } else if (ts.isFunctionDeclaration(node)) {
               const name = node.name;
-              if (name && isIdentifier(name) && name.text in replacements) {
+              if (name && isIdentifier(name) && replacements.hasOwnProperty(name.text)) {
                 const key = name.text as keyof typeof replacements;
                 return astNodes(replacements[key]);
               }
