e7dbe6740a06d9e077ce99b7fefbdcb8de8b59b8
Jeroen Engels <jfm.engels@gmail.com>
2021-12-06 00:07:03 +0100

    Remove dollar signs f

--- test/lambdaifyFunctionComposition.test.ts
+++ test/lambdaifyFunctionComposition.test.ts
@@ -6,11 +6,11 @@ import { lambdaifyFunctionComposition } from '../src/transforms/lambdaifyFunctio
 test('it can replace << by an anonymous function', () => {
   // Corresponds to f1 << f2
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeL, $f1, $f2);
+  var fn = A2($elm$core$Basics$composeL, f1, f2);
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a0) { return $f1($f2(_a0)) };
+  var fn = function (_a0) { return f1(f2(_a0)) };
   `;
 
   const { actual, expected } = transformCode(
@@ -25,11 +25,11 @@ test('it can replace << by an anonymous function', () => {
 test('it can replace >> by an anonymous function', () => {
   // Corresponds to f1 >> f2
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeR, $f1, $f2);
+  var fn = A2($elm$core$Basics$composeR, f1, f2);
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a0) { return $f2($f1(_a0)) };
+  var fn = function (_a0) { return f2(f1(_a0)) };
   `;
 
   const { actual, expected } = transformCode(
@@ -44,11 +44,11 @@ test('it can replace >> by an anonymous function', () => {
 test('it can replace nested function compositions', () => {
   // Corresponds to f1 >> f2 >> f3
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeR, $f1, A2($elm$core$Basics$composeR, $f2, $f3));
+  var fn = A2($elm$core$Basics$composeR, f1, A2($elm$core$Basics$composeR, f2, f3));
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a0) { return $f3($f2($f1(_a0))); };
+  var fn = function (_a0) { return f3(f2(f1(_a0))); };
   `;
 
   const { actual, expected } = transformCode(
