2615b182a86ea69271adec9b963796737ab1f2d6
Jeroen Engels <jfm.engels@gmail.com>
2021-12-05 12:21:16 +0100

    Split tests

--- test/lambdaifyFunctionComposition.test.ts
+++ test/lambdaifyFunctionComposition.test.ts
@@ -3,15 +3,31 @@ import ts from 'typescript';
 import { transformCode } from './helpers/transformCode';
 import { lambdaifyFunctionComposition } from '../src/transforms/lambdaifyFunctionComposition';
 
-test('it can replace a List.map on a "_List_fromArray" by a native map', () => {
+test('it can << by an anonymous function', () => {
   const initialCode = `
-  var left = A2($elm$core$Basics$composeL, $f1, $f2);
-  var right = A2($elm$core$Basics$composeR, $f1, $f2);
+  var fn = A2($elm$core$Basics$composeL, $f1, $f2);
   `;
 
   const expectedOutputCode = `
-  var left = function (_a0) { return $f2($f1(_a0)) };
-  var right = function (_a0) { return $f1($f2(_a0)) };
+  var fn = function (_a0) { return $f2($f1(_a0)) };
+  `;
+
+  const { actual, expected } = transformCode(
+    initialCode,
+    expectedOutputCode,
+    lambdaifyFunctionComposition
+  );
+
+  expect(actual).toBe(expected);
+});
+
+test('it can >> by an anonymous function', () => {
+  const initialCode = `
+  var fn = A2($elm$core$Basics$composeR, $f1, $f2);
+  `;
+
+  const expectedOutputCode = `
+  var fn = function (_a0) { return $f1($f2(_a0)) };
   `;
 
   const { actual, expected } = transformCode(
