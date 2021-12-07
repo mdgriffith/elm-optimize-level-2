3611d53fb743691f941bfd6d91c0b66eaaf1959b
Jeroen Engels <jfm.engels@gmail.com>
2021-12-07 21:47:18 +0100

    Wrap test code in IIFE

--- test/lambdaifyFunctionComposition.test.ts
+++ test/lambdaifyFunctionComposition.test.ts
@@ -6,11 +6,15 @@ import { lambdaifyFunctionComposition } from '../src/transforms/lambdaifyFunctio
 test('it can replace << by an anonymous function', () => {
   // Corresponds to: f1 << f2
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeL, f1, f2);
+  (function() {
+    var fn = A2($elm$core$Basics$composeL, f1, f2);
+  })()
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a_1) { return f1(f2(_a_1)) };
+  (function() {
+    var fn = function (_a_1) { return f1(f2(_a_1)) };
+  })()
   `;
 
   const { actual, expected } = transformCode(
@@ -25,11 +29,15 @@ test('it can replace << by an anonymous function', () => {
 test('it can replace >> by an anonymous function', () => {
   // Corresponds to: f1 >> f2
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeR, f1, f2);
+  (function() {
+    var fn = A2($elm$core$Basics$composeR, f1, f2);
+  })()
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a_1) { return f2(f1(_a_1)) };
+  (function() {
+    var fn = function (_a_1) { return f2(f1(_a_1)) };
+  })()
   `;
 
   const { actual, expected } = transformCode(
@@ -44,11 +52,15 @@ test('it can replace >> by an anonymous function', () => {
 test('it can replace nested function compositions with >>', () => {
   // Corresponds to: f1 >> f2 >> f3
   const initialCode = `
-  var fn = A2($elm$core$Basics$composeR, f1, A2($elm$core$Basics$composeR, f2, f3));
+  (function() {
+    var fn = A2($elm$core$Basics$composeR, f1, A2($elm$core$Basics$composeR, f2, f3));
+  })()
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a_1) { return f3(f2(f1(_a_1))); };
+  (function() {
+    var fn = function (_a_1) { return f3(f2(f1(_a_1))); };
+  })()
   `;
 
   const { actual, expected } = transformCode(
@@ -63,14 +75,18 @@ test('it can replace nested function compositions with >>', () => {
 test('it can replace nested function compositions with <<', () => {
   // Corresponds to: f3 << f2 << f1
   const initialCode = `
+  (function() {
   var fn = A2(
 		$elm$core$Basics$composeL,
 		A2($elm$core$Basics$composeL, f3, f2),
 		f1);
+  })()
   `;
 
   const expectedOutputCode = `
-  var fn = function (_a_1) { return f3(f2(f1(_a_1))); };
+  (function() {
+    var fn = function (_a_1) { return f3(f2(f1(_a_1))); };
+  })()
   `;
 
   const { actual, expected } = transformCode(
@@ -85,17 +101,21 @@ test('it can replace nested function compositions with <<', () => {
 test("When multiple new variables are introduced, they don't share the same name", () => {
   // Corresponds to: f >> List.map (g >> h)
   const initialCode = `
-  var fn = A2(
-    $elm$core$Basics$composeR,
-    f1,
-    $elm$core$List$map(
-      A2($elm$core$Basics$composeR, f2, f3)));
+  (function() {
+    var fn = A2(
+      $elm$core$Basics$composeR,
+      f1,
+      $elm$core$List$map(
+        A2($elm$core$Basics$composeR, f2, f3)));
+  })()
   `;
 
   // TODO Output for this could probably be improved
   const expectedOutputCode = `
-  var _b_1 = $elm$core$List$map(function (_a_2) { return f3(f2(_a_2)); });
-  var fn = function (_a_1) { return _b_1(f1(_a_1)); };
+  (function() {
+    var _b_1 = $elm$core$List$map(function (_a_2) { return f3(f2(_a_2)); });
+    var fn = function (_a_1) { return _b_1(f1(_a_1)); };
+  })()
   `;
 
   const { actual, expected } = transformCode(
