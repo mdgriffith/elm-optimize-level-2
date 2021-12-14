22f96b0213eb35d627d8d39ad9c0dc1cfec47975
Jeroen Engels <jfm.engels@gmail.com>
2021-12-14 23:35:43 +0100

    Add test

--- test/operationsFusion.test.ts
+++ test/operationsFusion.test.ts
@@ -27,3 +27,30 @@ test('it can fuse together List.map calls', () => {
 
   expect(actual).toBe(expected);
 });
+
+test('it can fuse together List.filter calls', () => {
+  // Corresponds to: f2 << f1
+  const initialCode = `
+  (function() {
+    var fn = function (x) {
+      return A2($elm$core$List$filter, f2, A2($elm$core$List$filter, f1, x));
+    };
+  })()
+  `;
+
+  const expectedOutputCode = `
+  (function() {
+    var fn = function (x) {
+      return A2($elm$core$List$filter, A2($elm$core$Basics$composeR, f1, f2), x);
+    };
+  })()
+  `;
+
+  const { actual, expected } = transformCode(
+    initialCode,
+    expectedOutputCode,
+    operationsFusion
+  );
+
+  expect(actual).toBe(expected);
+});
