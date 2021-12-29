2c443972fc5325a6e4adfbb610339f8946b9388a
Jeroen Engels <jfm.engels@gmail.com>
2021-12-29 19:17:26 +0100

    Add failing test

--- test/tailCallRecursion.test.ts
+++ test/tailCallRecursion.test.ts
@@ -105,6 +105,107 @@ test('it can turn a function that is tail-call recursive into a while loop', () 
   expect(actual).toBe(expected);
 });
 
+test('should re-use the label and while loop if there already is one', () => {
+  // Corresponds to the following Elm code
+  // recursiveFunction : (a -> b) -> List a -> List b -> List b
+  // recursiveFunction mapper list acc =
+  //     case list of
+  //         [] ->
+  //             acc
+  //         x :: xs ->
+  //             if cond then
+  //                recursiveFunction mapper xs (mapper x :: acc)
+  //             else
+  //                recursiveFunction mapper xs <| (mapper x :: acc)
+  const initialCode = `
+  var something$recursiveFunction = F3(
+	function (mapper, list, acc) {
+		recursiveFunction:
+		while (true) {
+			if (!list.b) {
+				return acc;
+			} else if (cond) {
+				var x = list.a;
+				var xs = list.b;
+				var $temp$mapper = mapper,
+					$temp$list = xs,
+					$temp$acc = A2(
+					$elm$core$List$cons,
+					mapper(x),
+					acc);
+				mapper = $temp$mapper;
+				list = $temp$list;
+				acc = $temp$acc;
+				continue recursiveFunction;
+            } else {
+                return A3(
+                    something$recursiveFunction,
+                    mapper,
+                    xs,
+                    A2(
+                        $elm$core$List$cons,
+                        mapper(x),
+                        acc));
+            }
+		}
+	});
+  `;
+
+  // Corresponds to the following TCO-ed Elm code
+  // recursiveFunction : (a -> b) -> List a -> List b -> List b
+  // recursiveFunction mapper list acc =
+  //     case list of
+  //         [] ->
+  //             acc
+  //         x :: xs ->
+  //             recursiveFunction mapper xs (mapper x :: acc)
+  const expectedOutputCode = `
+  var something$recursiveFunction = F3(
+	function (mapper, list, acc) {
+		recursiveFunction:
+		while (true) {
+			if (!list.b) {
+				return acc;
+			} else if (cond) {
+				var x = list.a;
+				var xs = list.b;
+				var $temp$mapper = mapper,
+					$temp$list = xs,
+					$temp$acc = A2(
+					$elm$core$List$cons,
+					mapper(x),
+					acc);
+				mapper = $temp$mapper;
+				list = $temp$list;
+				acc = $temp$acc;
+				continue recursiveFunction;
+            } else {
+				var x = list.a;
+				var xs = list.b;
+				var $temp$mapper = mapper,
+					$temp$list = xs,
+					$temp$acc = A2(
+					$elm$core$List$cons,
+					mapper(x),
+					acc);
+				mapper = $temp$mapper;
+				list = $temp$list;
+				acc = $temp$acc;
+				continue recursiveFunction;
+            }
+		}
+	});
+  `;
+
+  const { actual, expected } = transformCode(
+    initialCode,
+    expectedOutputCode,
+    createTailCallRecursionTransformer
+  );
+
+  expect(actual).toBe(expected);
+});
+
 test('should not change non-recursive functions', () => {
   const initialCode = `
   var something$recursiveFunction = F3(
