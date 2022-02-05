6807c0c5101a0fef9f702ba486db10b9a97224ce
Coury Ditch <cmditch@gmail.com>
2022-02-05 16:52:44 -0700

    Expose `transform: (jsSource : string, o3Enabled?: boolean) => string`.
    Useful for build tools (snowpack, webpack, parcel, etc.) which want a synchronous `string -> string` transform function for plugins compiling Elm projects.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as Run from './run';
+import * as Transform from './transform'
+import { toolDefaults } from "./types";
 
 export async function run(options: {
   inputFilePath: string | undefined,
@@ -18,3 +20,26 @@ export async function run(options: {
     () => { }
   );
 }
+
+/**
+ * Transform JS source (compiled output of `elm make`)
+ * Handy for making build tool plugins (e.g, parcel, snowpack, webpack, etc.)
+ */
+export function transform(
+  jsSource: string,
+  o3Enabled = false,
+): string {
+
+  if (jsSource == '') {
+    throw new Error('elm-optimize-level-2: JS source is empty.');
+  }
+
+  return Transform.transform(
+    "unused dirName param",
+    jsSource,
+    undefined,
+    false,
+    toolDefaults(o3Enabled, null)
+  )
+}
+
