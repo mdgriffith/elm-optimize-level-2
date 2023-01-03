3af04e99dfd4ba8927e99a588826930069f9f8d6
Jeroen Engels <jfm.engels@gmail.com>
2023-01-03 20:45:20 +0100

    Detect AX calls

--- src/transforms/supportArraysForHtml.ts
+++ src/transforms/supportArraysForHtml.ts
@@ -5,11 +5,22 @@ TODO Docs
 */
 
 import ts from 'typescript';
+import {parseAXFunction} from "./utils/ElmWrappers";
 
 
 export const supportArraysForHtml : ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
     const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+      if (ts.isCallExpression(node)
+        && ts.isIdentifier(node.expression)
+        && node.arguments.length > 0
+      ) {
+        const callExpression = node.expression;
+        const arity = parseAXFunction(callExpression.text);
+        if (arity) {
+        } else {
+        }
+      }
       return ts.visitEachChild(node, visitor, context);
     };
 
